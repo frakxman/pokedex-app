0cd60baa06ae7c52809bd04f463605eb0b23b31f
Fabian Rivera <frakxman@hotmail.com>
2025-03-14 21:52:50 -0500

    First components and structure

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,42 @@
+// src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import WelcomeView from '../views/WelcomeView.vue'
+import ListView from '../views/ListView.vue'
+import FavoritesView from '../views/FavoritesView.vue'
+import DetailView from '../views/DetailView.vue'
+import ErrorView from '../views/ErrorView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'welcome',
+    component: WelcomeView
+  },
+  {
+    path: '/list',
+    name: 'list',
+    component: ListView
+  },
+  {
+    path: '/favorites',
+    name: 'favorites',
+    component: FavoritesView
+  },
+  {
+    path: '/pokemon/:id',
+    name: 'detail',
+    component: DetailView
+  },
+  {
+    path: '/error',
+    name: 'error',
+    component: ErrorView
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+  history: createWebHistory(),
+  routes
 })
 
 export default router
---
+++ src/views/ErrorView.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+      <h1>Error Page</h1>
+      <p>Something went wrong.</p>
+    </div>
+  </template>
+  
